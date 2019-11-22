76bd29735964f4679b31a9ef7ed65e41afe7edf3
unknown <milena.micheloni@verity.local>
2019-11-22 11:21:47 -0300

    Commit da aplicação

---
+++ CorreiosApp/Data/Endereco.cs
@@ -0,0 +1,11 @@
+﻿namespace CorreiosAPP.Data
+{
+    public class Endereco
+    {
+        public string Logradouro { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+    }
+}
